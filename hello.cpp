5b52b1f19b9ced0849d13ccfd7e4bce4cf593abe
Yogesh Ag <yogeshmario102@gmail.com>
2021-11-04 00:55:43 +0530

    edited hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
-#include<bots/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-cout<<"hello ji! kaiso ho"<<endl;
-return 0;
+	cout<<"Nameste Duniya! Mai hu Ninja Hattori"<<endl;
+	return 0;
 }
